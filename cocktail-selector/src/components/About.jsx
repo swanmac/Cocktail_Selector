5a72dbbbd05b6bbaf08f6dd87c185f9c2673c1ee
Greg Swan <gmswan99@gmail.com>
2022-11-09 10:48:52 -0500

    Add Back Home Button to About

--- cocktail-selector/src/components/About.jsx
+++ cocktail-selector/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
@@ -8,6 +9,11 @@ export default function About() {
         Thank you for visiting our site, The Cocktail Selector. In the search box, just enter a few letters in the name of your favorite adult beverage and you will see a list of suggested beverages. Select the details button on any of the cocktails and you will be provided with additional details about the beverage including: the category, the cocktail glass, ingredients and instructions on how to prepare the beverage. Enjoy!!
     
       </p>
+      <div className='backHome'>
+      <Link to='/' className='btn btn-primary'>
+          back home
+        </Link>
+      </div>
     </section>
   );
 }
